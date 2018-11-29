2fa3e5414fe774ce11589c4ad95296d0bba55b3f
Guillaume Monet <Guillaume Monet@MSI>
2018-11-30 00:20:24 +0100

    Move probe, add probes manager

---
+++ src/main/java/fr/eloane/javamas/kernel/probes/Probe.java
@@ -0,0 +1,60 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2018 Guillaume Monet.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fr.eloane.javamas.kernel.probes;
+
+import java.io.Serializable;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Think about something hot because it'll getting cold
+ *
+ * @author Guillaume Monet
+ * @param <T>
+ */
+public abstract class Probe<T> implements Serializable, Observer {
+
+    private static final long serialVersionUID = 7523662972689640778L;
+
+    /**
+     *
+     * @param o
+     * @param arg
+     */
+    @Override
+    public final void update(Observable o, Object arg) {
+        try {
+            this.handleProbe((T) arg);
+        } catch (ClassCastException e) {
+        }
+
+    }
+
+    /**
+     *
+     * @param value
+     */
+    protected abstract void handleProbe(T value);
+
+}
